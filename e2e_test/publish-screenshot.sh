b20d3f29cf0a4620c438fdc409ac4c8d5159a94c
Hidetake Iwata <int128@gmail.com>
2021-02-21 12:59:32 +0900

    Publish screenshot to Wiki (#180)
    * Publish screenshot to Wiki
    
    * Update e2e-test.yaml
    
    * Update publish-screenshot.sh

---
+++ e2e_test/publish-screenshot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -eux
+
+wiki_dir="$(mktemp -d)"
+screenshot_path="$GITHUB_REF/screenshot.png"
+comment_body="## e2e-test
+![screenshot](https://github.com/$GITHUB_REPOSITORY/wiki/$screenshot_path)"
+
+# publish the screenshot
+git clone --depth=1 "https://x:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git" "$wiki_dir"
+mkdir -p $(dirname "$wiki_dir/$screenshot_path")
+cp output/screenshot.png "$wiki_dir/$screenshot_path"
+git -C "$wiki_dir" commit -a -m "ci-publish-screenshot: $GITHUB_REF"
+git -C "$wiki_dir" push origin HEAD
+
+# comment it to the pull request
+if [ "$GITHUB_HEAD_REF" ]; then
+  gh pr comment "$GITHUB_HEAD_REF" --body "$comment_body"
+fi
